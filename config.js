184f0c1d03f2b0f35bfe7ddadb470a9bafde4ad4
Nate Murray <nate@natemurray.com>
2018-12-21 17:14:40 -0600

    added 30 days of vue config

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: `30 Days of Vue`,
+  subtitle: `An introduction to Vue - In 30 bite-size morsels`,
+  heroPhotoUrl: 'todo.jpg',
+  paperBookPhotoUrl: 'todo.jpg',
+  publicLessonCount: 7,
+  summary: `
+Interested in learning Vue but having trouble getting started?
+
+We'll teach you how it all works - _for free_
+
+Over the next 30 days, we'll walk through everything you need to know to work with Vue. From the **very beginning** through **testing and deployment** of our first app.
+`,
+  pdfLength: '310+',
+  faq: [
+    {
+      q: 'What is this?',
+      a:
+        'This course is a series of articles which teach you how to use Vue from the ground up. The series provides a step-by-step process you can use to learn Vue from an empty folder to a deployed Vue app.',
+    },
+    {
+      q: 'What if I get stuck?',
+      a:
+        "Just [send us an email](us@fullstack.io) and we'll be happy to help you get unstuck",
+    },
+    {
+      q: 'Who wrote this?',
+      a:
+        "We're the authors of the [Fullstack Vue Book](https://fullstack.io/vue)",
+    },
+  ],
+  faqImageUrl: 'todo.jpg',
+  faqCta: 'Enroll in this free course',
+  courseAuthor: 'hassan',
+  closingCtaH1: 'Get started now',
+  closingCtaCopy: `Join us on our 30-day journey in Vue. Join thousands of other professional Vue developers and learn one of the most powerful web application development frameworks available today.`,
+  closingCtaButton: 'Download 30 Days of Vue PDF (FREE)',
+};
